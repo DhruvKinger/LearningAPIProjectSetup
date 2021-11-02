65110cf69ad97c9df7e6bd3736d8e6047e431793
Dhruv Kinger <cse1785@gmail.com>
2021-11-02 23:53:19 +0530

    creating api

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    [ApiController]
+    [Route("api/commands")]
+    public class CommandsController : Controller
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+       
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItems = _repository.GetCommandById(id);
+            return Ok(commandItems);
+        }
+
+
+    }
+}
